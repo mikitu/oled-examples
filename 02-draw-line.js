364b9060beeb06b081a7148c0f0bbbce3dc4d45d
mikitu <19qywter78>
2018-03-10 08:19:37 +0000

    example 2 - draw line

---
+++ 02-draw-line.js
@@ -0,0 +1,23 @@
+var i2c = require('i2c-bus'),
+    Oled = require('oled-i2c-bus'),
+    font = require('oled-font-5x7');
+
+const SIZE_X=128,
+    SIZE_Y=32,
+    CHANNEL = 1
+;
+var i2cBus = i2c.openSync(CHANNEL)
+
+var opts = {
+    width: SIZE_X,
+    height: SIZE_Y,
+    address: 0x3C
+};
+
+var oled = new Oled(i2cBus, opts);
+
+
+oled.clearDisplay();
+oled.turnOnDisplay();
+
+oled.drawLine(1, 1, 128, 32, 1);
